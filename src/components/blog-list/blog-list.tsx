3cb5a506536f95008b0cb8a0b93a8b134066dae1
Zach Olivare <zach@olivare.net>
2023-09-11 15:21:05 -0500

    fix: Make Astro content ignore TSX helpers

--- src/components/blog-list/blog-list.tsx
+++ src/components/blog-list/blog-list.tsx
@@ -8,12 +8,7 @@ export function BlogList(props: BlogListProps) {
 
   return (
     <ol
-      className={cs(
-        'BlogList list-none',
-        'flex flex-wrap justify-center gap-8',
-        // 'grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 grid-rows-2 gap-8 justify-items-center',
-        className,
-      )}
+      className={cs('BlogList list-none', 'flex flex-wrap justify-center gap-8 pb-72', className)}
     >
       {children}
     </ol>
