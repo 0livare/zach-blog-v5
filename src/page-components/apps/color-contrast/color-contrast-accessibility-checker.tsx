940e867e020295f6e41c6a974d88f3213a06d584
Zach Olivare <zach@olivare.net>
2023-11-04 16:37:23 -0500

    feat: Create UI for contrast pass/fail

--- src/page-components/apps/color-contrast/color-contrast-accessibility-checker.tsx
+++ src/page-components/apps/color-contrast/color-contrast-accessibility-checker.tsx
@@ -22,8 +22,15 @@ const stripeBackgroundImage =
 const stickOutHeight = 66
 
 export function ColorContrastAccessibilityChecker(props: React.ComponentProps<'div'>) {
-  const [colors, setColors] = React.useState(['#ff0000', '#00ff00', '#0000ff'])
-  const finalColor = React.useMemo(() => calculateImageComposite(colors), [colors])
+  const [colors, setColors] = React.useState(['#fff', 'rgb(0, 0, 0, 0.5)'])
+  const bgColor = React.useMemo(() => calculateImageComposite(colors.slice(0, -1)), [colors])
+  const textColor = React.useMemo(() => calculateImageComposite(colors), [colors])
+  const bgColorStr = bgColor?.rgb().string()
+  const textColorStr = textColor?.rgb().string()
+  const contrast = React.useMemo(() => {
+    if (!bgColor || !textColor) return '--'
+    return bgColor.contrast(textColor).toFixed(2)
+  }, [bgColor, textColor])
 
   function handleAddLayer() {
     setColors((prev) => [...prev, `#${randomColor()}`])
@@ -39,35 +46,85 @@ export function ColorContrastAccessibilityChecker(props: React.ComponentProps<'d
 
   return (
     <div {...props}>
-      <div style={{height: stickOutHeight * colors.length, position: 'relative'}}>
-        {colors.map((color, index) => (
-          <Layer
-            key={index}
-            color={color}
-            index={index}
-            length={colors.length}
-            onColorChange={(i, color) => {
-              setColors((prev) => {
-                const next = [...prev]
-                next[i] = color
-                return next
-              })
-            }}
-            onAddLayer={handleAddLayer}
-            onRemoveLayer={handleRemoveLayer}
-          />
-        ))}
+      <div className="flex flex-wrap gap-8">
+        <div
+          style={{height: stickOutHeight * colors.length, position: 'relative'}}
+          className="basis-96 grow-[3]"
+        >
+          {colors.map((color, index) => (
+            <Layer
+              key={index}
+              color={color}
+              index={index}
+              length={colors.length}
+              onColorChange={(i, color) => {
+                setColors((prev) => {
+                  const next = [...prev]
+                  next[i] = color
+                  return next
+                })
+              }}
+              onAddLayer={handleAddLayer}
+              onRemoveLayer={handleRemoveLayer}
+            />
+          ))}
+        </div>
+        <div className="self-end grow basis-24 text-left flex flex-col gap-4">
+          Example Text:
+          <Demo className="p-8 text-xl" bgColorStr={bgColorStr} textColorStr={textColorStr}>
+            Hello World
+          </Demo>
+        </div>
       </div>
 
-      <div
+      <div className="flex flex-wrap gap-8">
+        <div className="p-8 mt-16 border-2 border-green-400 inline-flex flex-col items-center self-center">
+          <div className="text-lg">Contrast Ratio</div>
+          <div className="text-3xl">{contrast} : 1</div>
+        </div>
+        <div className="text-left py-8">
+          <h2 className="text-3xl text-tprimary block border-b border-tprimary mt-4 mb-2">
+            Normal Text
+          </h2>
+          <div className="flex gap-8">
+            <div>
+              <div>WCAG AA: Pass</div>
+              <div>WCAG AAA:Pass</div>
+            </div>
+            <Demo bgColorStr={bgColorStr} textColorStr={textColorStr} />
+          </div>
+          <h2 className="text-3xl text-tprimary block border-b border-tprimary mt-4 mb-2">
+            Large Text
+          </h2>
+          WCAG AA:Pass WCAG AAA:Pass The five boxing wizards jump quickly.
+          <h2 className="text-3xl text-tprimary block border-b border-tprimary mt-4 mb-2">
+            Graphical Objects and User Interface Components
+          </h2>
+          WCAG AA:
+        </div>
+      </div>
+      {/* <div
         className="h-[200px] w-full"
         style={{
-          backgroundColor: finalColor,
-          color: safeParseColor(finalColor)?.isDark() ? 'white' : 'black',
+          backgroundColor: textColor,
+          color: safeParseColor(textColor)?.isDark() ? 'white' : 'black',
         }}
       >
-        <h2>Final Color {finalColor}</h2>
-      </div>
+        <h2>Composite Color {textColor}</h2>
+      </div> */}
+    </div>
+  )
+}
+
+function Demo(props: React.ComponentProps<'div'> & {bgColorStr: string; textColorStr: string}) {
+  const {bgColorStr, textColorStr, className, children, ...rest} = props
+  return (
+    <div
+      {...rest}
+      className={twMerge('border border-black/20 p-4 text-center', className)}
+      style={{backgroundColor: bgColorStr, color: textColorStr}}
+    >
+      {children || 'The five boxing wizards jump quickly.'}
     </div>
   )
 }
@@ -93,6 +150,10 @@ function Layer(props: any) {
         backgroundImage: isColorValid ? undefined : stripeBackgroundImage,
       }}
     >
+      <p className="text-xs absolute left-4 top-2" style={{color: accentColor}}>
+        {index === length - 1 ? 'Text Color' : 'Background Color ' + (index + 1)}
+      </p>
+
       <input
         type="text"
         value={color}
@@ -102,9 +163,11 @@ function Layer(props: any) {
 
       {index === length - 1 && (
         <div className="absolute bottom-0 right-0">
-          <IconButton hoverColor={hoverColor} onClick={() => onRemoveLayer(index)}>
-            <Icon name="Trash" color={accentColor} />
-          </IconButton>
+          {length > 2 && (
+            <IconButton hoverColor={hoverColor} onClick={() => onRemoveLayer(index)}>
+              <Icon name="Trash" color={accentColor} />
+            </IconButton>
+          )}
           <IconButton hoverColor={hoverColor} onClick={onAddLayer}>
             <Icon name="Plus" color={accentColor} />
           </IconButton>
@@ -143,5 +206,5 @@ function calculateImageComposite(colors: string[]) {
     }
   })
 
-  return safeParseColor(compositeColor)?.rgb().string()
+  return safeParseColor(compositeColor)
 }
