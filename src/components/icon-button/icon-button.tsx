9ab29a97dba2d0c69e07f45489031cfa0d4ad306
Zach Olivare <zach@olivare.net>
2023-11-04 13:45:58 -0500

    feat: Write a color composite w/alpha fn

--- src/components/icon-button/icon-button.tsx
+++ src/components/icon-button/icon-button.tsx
@@ -1,10 +1,14 @@
 import React from 'react'
 import {twMerge as cs} from 'tailwind-merge'
+import styles from './icon-button.module.scss'
 
-export type IconButtonProps = React.ComponentProps<'button'> & {size?: number | string}
+export type IconButtonProps = React.ComponentProps<'button'> & {
+  size?: number | string
+  hoverColor?: string
+}
 
 export function IconButton(props: IconButtonProps) {
-  let {className, children, style = {}, size = 24, ...rest} = props
+  let {className, children, style = {}, size = 24, hoverColor, ...rest} = props
 
   return (
     <button
@@ -13,9 +17,10 @@ export function IconButton(props: IconButtonProps) {
         'm-0, rounded-full p-[0.4em] border-0 transition-all disabled:cursor-not-allowed',
         'bg-transparent hover:bg-slate-600/20 dark:hover:bg-slate-100/30',
         'active:bg-slate-600/40 dark:active:bg-slate-100/50',
+        hoverColor && styles.overrideHover,
         className,
       )}
-      style={{fontSize: size, ...style}}
+      style={{fontSize: size, ...style, '--iconBtnHoverBgColor': hoverColor} as React.CSSProperties}
       {...rest}
     >
       {children}
--- src/page-components/apps/color-contrast/color-contrast-accessibility-checker.tsx
+++ src/page-components/apps/color-contrast/color-contrast-accessibility-checker.tsx
@@ -3,16 +3,7 @@ import Color from 'color'
 import {twMerge} from 'tailwind-merge'
 import {Icon, IconButton} from '~/components'
 
-function isValidColor(color: string) {
-  try {
-    Color(color)
-    return true
-  } catch {
-    return false
-  }
-}
-
-function safeColor(color: Parameters<typeof Color>[0]) {
+function safeParseColor(color: Parameters<typeof Color>[0]) {
   try {
     return Color(color)
   } catch {
@@ -20,36 +11,59 @@ function safeColor(color: Parameters<typeof Color>[0]) {
   }
 }
 
+const MAX_COLOR = Number('0xffffffff')
+function randomColor() {
+  return Math.floor(Math.random() * MAX_COLOR).toString(16)
+}
+
 const stripeBackgroundImage =
   'repeating-linear-gradient(45deg, #606dbc, #606dbc 10px, #465298 10px, #465298 20px)'
 
+const stickOutHeight = 66
+
 export function ColorContrastAccessibilityChecker(props: React.ComponentProps<'div'>) {
   const [colors, setColors] = React.useState(['#ff0000', '#00ff00', '#0000ff'])
-  const finalColor = React.useMemo(() => calculateFinalColor(colors), [colors])
+  const finalColor = React.useMemo(() => calculateImageComposite(colors), [colors])
+
+  function handleAddLayer() {
+    setColors((prev) => [...prev, `#${randomColor()}`])
+  }
+
+  function handleRemoveLayer(index: number) {
+    setColors((prev) => {
+      const next = [...prev]
+      next.splice(index, 1)
+      return next
+    })
+  }
 
   return (
     <div {...props}>
-      {colors.map((color, index) => (
-        <Layer
-          key={index}
-          color={color}
-          index={index}
-          length={colors.length}
-          onColorChange={(i, color) => {
-            setColors((prev) => {
-              const next = [...prev]
-              next[i] = color
-              return next
-            })
-          }}
-        />
-      ))}
+      <div style={{height: stickOutHeight * colors.length, position: 'relative'}}>
+        {colors.map((color, index) => (
+          <Layer
+            key={index}
+            color={color}
+            index={index}
+            length={colors.length}
+            onColorChange={(i, color) => {
+              setColors((prev) => {
+                const next = [...prev]
+                next[i] = color
+                return next
+              })
+            }}
+            onAddLayer={handleAddLayer}
+            onRemoveLayer={handleRemoveLayer}
+          />
+        ))}
+      </div>
 
       <div
         className="h-[200px] w-full"
         style={{
           backgroundColor: finalColor,
-          color: safeColor(finalColor)?.isDark() ? 'white' : 'black',
+          color: safeParseColor(finalColor)?.isDark() ? 'white' : 'black',
         }}
       >
         <h2>Final Color {finalColor}</h2>
@@ -59,31 +73,24 @@ export function ColorContrastAccessibilityChecker(props: React.ComponentProps<'d
 }
 
 function Layer(props: any) {
-  const {color, index, onColorChange, className, length, ...rest} = props
-  const isColorValid = isValidColor(color)
-
-  function heightForIndex(index: number) {
-    return (length - index) * 60 + 50
-  }
-
-  const thisHeight = heightForIndex(index)
-  const prevIndices = Array.from({length: index}, (_, i) => i)
-  const heightToMove = prevIndices.reduce((acc, i) => acc + heightForIndex(i) - 50, 0)
+  const {color, index, onColorChange, className, length, onAddLayer, onRemoveLayer, ...rest} = props
+  const colorObj = safeParseColor(color)
+  const isColorValid = !!colorObj
+  const accentColor = colorObj?.isDark() ? 'white' : 'black'
+  const hoverColor = Color(accentColor).alpha(0.2).rgb().toString()
 
   return (
     <div
       {...rest}
       className={twMerge('p-2', className)}
       style={{
-        marginTop: index * 8,
-        marginLeft: index * 16,
-        width: `calc(100% - ${16 * 2 * index}px)`,
+        position: 'absolute',
+        top: index * stickOutHeight,
+        left: index * 16,
+        right: index * 16,
+        height: stickOutHeight * (length - index),
         backgroundColor: color,
         backgroundImage: isColorValid ? undefined : stripeBackgroundImage,
-        transform: index == 0 ? 0 : `translateY(-${heightToMove}px)`,
-        height: thisHeight,
-        position: 'relative',
-        // position: index === 0 ? 'relative' : 'absolute',
       }}
     >
       <input
@@ -95,11 +102,11 @@ function Layer(props: any) {
 
       {index === length - 1 && (
         <div className="absolute bottom-0 right-0">
-          <IconButton>
-            <Icon name="Trash" />
+          <IconButton hoverColor={hoverColor} onClick={() => onRemoveLayer(index)}>
+            <Icon name="Trash" color={accentColor} />
           </IconButton>
-          <IconButton>
-            <Icon name="Plus" />
+          <IconButton hoverColor={hoverColor} onClick={onAddLayer}>
+            <Icon name="Plus" color={accentColor} />
           </IconButton>
         </div>
       )}
@@ -107,38 +114,34 @@ function Layer(props: any) {
   )
 }
 
-type MaybeAlphaRgb = {r: number; g: number; b: number; alpha?: number}
-type RGBObj = Required<MaybeAlphaRgb>
+type RGBObj = {r: number; g: number; b: number; alpha: number}
 
-function calculateFinalColor(colors: string[]) {
-  const rgbas = colors
+/**
+ * Using the Alpha compositing "over" method with premultiplied alpha
+ * See: https://en.wikipedia.org/wiki/Alpha_compositing
+ */
+function calculateImageComposite(colors: string[]) {
+  let rgbas = colors
     .map((color) => {
-      const obj = safeColor(color)?.rgb().object() as MaybeAlphaRgb | null
+      const obj = safeParseColor(color)?.rgb().object() as RGBObj | null
       if (!obj) return null
       if (obj.alpha) return obj as RGBObj
       return {...obj, alpha: 1} as RGBObj
     })
-    .filter(Boolean) as RGBObj[]
+    .filter(Boolean)
+    .filter((rgba) => rgba!.alpha ?? 1 > 0) as RGBObj[]
 
-  const visibleAlphas = rgbas.map((rgba, index, array) => {
-    let visibleAlpha = rgba.alpha
-    if (index < array.length - 1) {
-      visibleAlpha = 1 - array[index + 1].alpha
+  const compositeColor = rgbas.reduce((acc, rgba) => {
+    const {r, g, b, alpha} = rgba
+    const {r: bgR, g: bgG, b: bgB, alpha: bgAlpha} = acc
+
+    return {
+      r: r * alpha + bgR * (1 - alpha),
+      g: g * alpha + bgG * (1 - alpha),
+      b: b * alpha + bgB * (1 - alpha),
+      alpha: alpha + bgAlpha * (1 - alpha),
     }
-    return {...rgba, alpha: visibleAlpha}
   })
 
-  const finalColor = visibleAlphas
-    .map((rgba) => ({
-      r: rgba.r * rgba.alpha,
-      g: rgba.g * rgba.alpha,
-      b: rgba.b * rgba.alpha,
-    }))
-    .reduce((acc, curr) => ({
-      r: acc.r + curr.r,
-      g: acc.g + curr.g,
-      b: acc.b + curr.b,
-    }))
-
-  return safeColor(finalColor)?.rgb().string()
+  return safeParseColor(compositeColor)?.rgb().string()
 }
