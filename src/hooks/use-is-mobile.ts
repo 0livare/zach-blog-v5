66ea40361cecc2c521f655e5b9451d97bd6f82bd
Zach Olivare <zach@olivare.net>
2023-11-13 12:28:32 -0600

    fix: Assume desktop until media query loads

--- src/hooks/use-is-mobile.ts
+++ src/hooks/use-is-mobile.ts
@@ -2,8 +2,9 @@ import React from 'react'
 
 const MOBILE_WIDTH = 768
 
+/** Default: False (desktop) */
 export function useIsMobile() {
-  const [width, setWidth] = React.useState(0)
+  const [width, setWidth] = React.useState(Number.POSITIVE_INFINITY)
 
   React.useEffect(() => {
     new ResizeObserver(() => {
